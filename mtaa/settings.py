06676dbc1cf72575a5368bad2a400deca9667c2b
Nur tusa <nur.tusa@student.moringaschool.com>
2022-01-07 23:29:48 +0300

    fix

--- mtaa/settings.py
+++ mtaa/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -164,12 +165,9 @@ LOGOUT_URL='/'
 LOGIN_URL='/'
 LOGIN_REDIRECT_URL='/'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
